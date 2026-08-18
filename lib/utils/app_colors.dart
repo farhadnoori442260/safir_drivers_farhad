07b8b80c83edcf451e46638c86286bf7269c5b02
farhadnoori442260 <farhadnoori552@gmail.com>
2026-08-18 21:38:25 +0330

    Add SafirColors class for color constants

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SafirColors {
+  static const Color primaryButton = Color(0xFF1B7A57);
+  static const Color buttonPressed = Color(0xFF0F4A35);
+  static const Color cardBgLight = Color(0xFFEAF6F1);
+  static const Color successColor = Color(0xFF22C55E);
+  static const Color buttonTextColor = Color(0xFFFFFFFF);
+  static const Color brandDark = Color(0xFF145A41);
+}
