8489382208546d59d171ac99481503ecf9a094e3
farhadnoori442260 <farhadnoori552@gmail.com>
2026-08-19 10:24:09 +0330

    Change import from app_colors to utils/app_colors

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-import 'package:safir_drivers/constants/app_colors.dart'; // 📌 اتصال به پالت رنگی اصلی
+import 'package:safir_drivers/utils/app_colors.dart'; // 📌 اتصال به پالت رنگی اصلی
 import 'package:safir_drivers/pages/auth/register_screen.dart';
 import 'package:safir_drivers/pages/dashboard.dart';
 import 'package:safir_drivers/providers/authentication_provider.dart';
