605cbd6bd214b9ce0b965a7193f8a30185ca0cbb
farhadnoori442260 <farhadnoori552@gmail.com>
2026-08-20 01:17:45 +0330

    Add navigation_page import to home_page.dart

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:safir_drivers/pages/navigation/navigation_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
