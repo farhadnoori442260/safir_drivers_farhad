af4b24a23697a8cd61467b832c1883cf6b6788f6
farhadnoori442260 <farhadnoori552@gmail.com>
2026-08-19 16:24:01 +0430

    Fix import statement for cloud_firestore package

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package0cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
